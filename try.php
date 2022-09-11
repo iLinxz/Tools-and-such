ae063a5057e60ca4ef28c83a376eb15a4039a6ad
iLinxz <60783266+iLinxz@users.noreply.github.com>
2022-09-11 16:48:40 +0100

    Create try.php

---
+++ try.php
@@ -0,0 +1,18 @@
+<?php
+$mysqli = new mysqli("127.0.0.1","bssredteam","lkasfklasjklfjkl1248712894jlaLJALFASF3213","bssredteam");
+
+// Check connection
+if ($mysqli -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  exit();
+}
+
+// Perform query
+if ($result = $mysqli -> query("SHOW TABLES FROM bssredteam")) {
+  echo "Returned rows are: " . $result -> num_rows;
+  // Free result set
+  $result -> free_result();
+}
+
+$mysqli -> close();
+?>
