8e39befd914816f0cf3f1d7b2e2df3405b781da2
iLinxz <60783266+iLinxz@users.noreply.github.com>
2022-09-11 16:53:11 +0100

    Update and rename try1.php to try2.php

---
+++ try2.php
@@ -0,0 +1,17 @@
+<?php
+$con = mysqli_connect("127.0.0.1","bssredteam","lkasfklasjklfjkl1248712894jlaLJALFASF3213","bssredteam");
+
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  exit();
+}
+
+// Perform query
+if ($result = mysqli_query($con, "SHOW TABLES FROM bssredteam")) {
+  echo "Returned rows are: " . mysqli_num_rows($result);
+  // Free result set
+  mysqli_free_result($result);
+}
+
+mysqli_close($con);
+?>
