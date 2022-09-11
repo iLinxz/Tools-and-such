ade9d85d9ecd49f26c64c7c2a3e39b68944fc18b
iLinxz <60783266+iLinxz@users.noreply.github.com>
2022-09-11 16:44:44 +0100

    Update try.php

--- try.php
+++ try.php
@@ -1,26 +1,23 @@
 <?php
-$mysqli = new mysqli("127.0.0.1","bssredteam","lkasfklasjklfjkl1248712894jlaLJALFASF3213","bssredteam");
+$dbname = 'bssredteam';
 
-// Check connection
-if ($mysqli -> connect_errno) {
-  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-  exit();
+if (!mysql_connect('127.0.0.1', 'bssredteam', 'lkasfklasjklfjkl1248712894jlaLJALFASF3213')) {
+    echo 'Could not connect to mysql';
+    exit;
 }
 
-// Perform query
-if ($result = $mysqli -> query("show tables;")) {
-  echo "Returned rows are: " . $result -> num_rows;
-  // Free result set
-  $result -> free_result();
-}
+$sql = "SHOW TABLES FROM $dbname";
+$result = mysql_query($sql);
 
-echo '------------';
+if (!$result) {
+    echo "DB Error, could not list tables\n";
+    echo 'MySQL Error: ' . mysql_error();
+    exit;
+}
 
 while ($row = mysql_fetch_row($result)) {
     echo "Table: {$row[0]}\n";
 }
 
 mysql_free_result($result);
-
-$mysqli -> close();
 ?>
